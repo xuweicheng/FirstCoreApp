df5a02463d3a6a1ff81cc682fa6c603752a9d78e
xuweicheng <weicheng.xu@gmail.com>
2018-03-16 16:55:38 -0700

    add IGreeter.cs

---
+++ IGreeter.cs
@@ -0,0 +1,15 @@
+﻿namespace FirstCoreApp
+{
+    public interface IGreeter
+    {
+        string getGreetingOfToday();
+    }
+
+    public class Greeter : IGreeter
+    {
+        public string getGreetingOfToday()
+        {
+            return "Greeting from Greeter.";
+        }
+    }
+}

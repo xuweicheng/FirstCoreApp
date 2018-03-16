453980d2f8b8c3fb599afcf94466103e66bcea9d
xuweicheng <weicheng.xu@gmail.com>
2018-03-16 15:38:42 -0700

    read from configuration, from appsettings.json or command line

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace FirstCoreApp
@@ -18,7 +19,9 @@ namespace FirstCoreApp
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, 
+                            IHostingEnvironment env,
+                            IConfiguration configuration)
         {
             if (env.IsDevelopment())
             {
@@ -27,7 +30,8 @@ namespace FirstCoreApp
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var greeting = configuration["Greeting"];
+                await context.Response.WriteAsync(greeting);
             });
         }
     }

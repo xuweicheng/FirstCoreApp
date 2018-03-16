a2620fcdd1b35e8294c86e8c7ee8bb138f775b1e
xuweicheng <weicheng.xu@gmail.com>
2018-03-16 16:57:14 -0700

    change Startup to use IGreeter

--- Startup.cs
+++ Startup.cs
@@ -16,12 +16,13 @@ namespace FirstCoreApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IGreeter, Greeter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, 
                             IHostingEnvironment env,
-                            IConfiguration configuration)
+                            IGreeter greeter)
         {
             if (env.IsDevelopment())
             {
@@ -30,8 +31,8 @@ namespace FirstCoreApp
 
             app.Run(async (context) =>
             {
-                var greeting = configuration["Greeting"];
-                await context.Response.WriteAsync(greeting);
+                var greetingMessage = greeter.getGreetingOfToday();
+                await context.Response.WriteAsync(greetingMessage);
             });
         }
     }

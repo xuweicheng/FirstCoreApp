b0f7ca50c31be2658de175ed72c811546da65fba
xuweicheng <weicheng.xu@gmail.com>
2018-03-19 14:36:29 -0700

    attribute routing for default, need to specifically list them

--- Controllers/AboutController.cs
+++ Controllers/AboutController.cs
@@ -4,11 +4,13 @@ using Microsoft.AspNetCore.Mvc;
 namespace FirstCoreApp.Controllers
 {
     //[Route("About")]
-    [Route("[controller]/[action=phone]")]
+    //[Route("[controller]/[action=phone]")] can't use the default convention in attribute routing, specifically list them out
+    [Route("[controller]/[action]")]
     public class AboutController 
     {
         //[Route("")]
         //[Route("[action]")]
+        [Route("[controller]")] //specifically list the default routing
         public string Phone()
         {
             return "123-456-7890";

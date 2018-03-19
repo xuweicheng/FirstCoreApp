fe1c467c6787927861c9ce8e69d4cacb7dde2b6d
xuweicheng <weicheng.xu@gmail.com>
2018-03-19 14:09:17 -0700

    add attribute routing for about controller

--- Controllers/AboutController.cs
+++ Controllers/AboutController.cs
@@ -1,13 +1,21 @@
 ﻿
+using Microsoft.AspNetCore.Mvc;
+
 namespace FirstCoreApp.Controllers
 {
+    //[Route("About")]
+    [Route("[controller]/[action=phone]")]
     public class AboutController 
     {
+        //[Route("")]
+        //[Route("[action]")]
         public string Phone()
         {
             return "123-456-7890";
         }
 
+        //[Route("Address")]
+        //[Route("[action]")]
         public string Address()
         {
             return "Canada";

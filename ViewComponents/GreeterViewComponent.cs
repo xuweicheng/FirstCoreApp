ac82365ff5c90068860a47a65bfeb539c6a8a6e1
xuweicheng <weicheng.xu@gmail.com>
2018-03-20 16:49:41 -0700

    add greeter view component

---
+++ ViewComponents/GreeterViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using FirstCoreApp.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstCoreApp.ViewComponents
+{
+    public class GreeterViewComponent : ViewComponent
+    {
+        private IGreeter _greeter;
+
+        public GreeterViewComponent(IGreeter greeter)
+        {
+            _greeter = greeter;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var greeting = _greeter.getGreetingOfToday();
+            return View("Default", greeting);
+        }
+    }
+}

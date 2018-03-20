e9e5b02f0c0ef42f5c8a1e0a2bb064d7ec8bcdf2
xuweicheng <weicheng.xu@gmail.com>
2018-03-20 15:47:50 -0700

    add Greeting razor page

---
+++ Pages/Greeting.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using FirstCoreApp.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FirstCoreApp.Pages
+{
+    public class GreetingModel : PageModel
+    {
+        private IGreeter _greeter;
+        public string Greeting { get; set; }
+
+        public GreetingModel(IGreeter greeter)
+        {
+            _greeter = greeter;
+        }
+
+        public void OnGet(string name)
+        {
+            Greeting = $"{name} : {_greeter.getGreetingOfToday()}";
+        }
+    }
+}
